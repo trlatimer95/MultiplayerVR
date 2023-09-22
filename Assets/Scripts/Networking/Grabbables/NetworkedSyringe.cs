a18f0360d5a9f73efa7f78d30e1daa6182fe26e5
Tyler Latimer <31456150+trlatimer95@users.noreply.github.com>
2023-09-21 22:58:49 -0700

    Implemented Syringe plunger movement over network

--- Assets/Scripts/Networking/Grabbables/NetworkedSyringe.cs
+++ Assets/Scripts/Networking/Grabbables/NetworkedSyringe.cs
@@ -1,8 +1,4 @@
 using Fusion;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 [OrderAfter(typeof(NetworkedGrabbable))]
@@ -14,6 +10,8 @@ public class NetworkedSyringe : NetworkBehaviour
     [SerializeField] float maxPlungerDrawValue;
     [SerializeField] float drawSpeed = 5.0f;
 
+    [Networked] private Vector3 localPosition { get; set; } = Vector3.zero;
+
     private NetworkedGrabbable grabbable;
     private NetworkHandGrabber currentGrabber;
     private NetworkRig currentGrabberRig;
@@ -21,18 +19,18 @@ public class NetworkedSyringe : NetworkBehaviour
     private float previousInputValue = 0f;
     private bool drawPlunger = true;
 
-    private void Awake()
+    public override void Spawned()
     {
-        grabbable = GetComponent<NetworkedGrabbable>();        
-    }
+        base.Spawned();
 
-    private void OnEnable()
-    {
+        grabbable = GetComponent<NetworkedGrabbable>();
         grabbable.onGrabChanged += GrabbableChanged;
     }
 
-    private void OnDisable()
+    public override void Despawned(NetworkRunner runner, bool hasState)
     {
+        base.Despawned(runner, hasState);
+
         grabbable.onGrabChanged -= GrabbableChanged;
     }
 
@@ -42,17 +40,17 @@ public class NetworkedSyringe : NetworkBehaviour
         {
             currentGrabberRig = grabbable.CurrentGrabber.GetComponentInParent<NetworkRig>();
             currentGrabber = grabbable.CurrentGrabber;
-        }          
+        }
         else
         {
             currentGrabberRig = null;
             currentGrabber = null;
-        }           
+        }
     }
 
     public override void FixedUpdateNetwork()
     {
-        if (currentGrabber == null) return;
+        if (currentGrabber == null || !grabbable.IsGrabbed) return;
 
         previousInputValue = triggerInputValue;
 
@@ -65,33 +63,41 @@ public class NetworkedSyringe : NetworkBehaviour
         // Reverse direction when trigger is released
         if (previousInputValue != 0 && triggerInputValue == 0)
             drawPlunger = !drawPlunger;
+
+        CalculatePlungerLocation();
     }
 
     public override void Render()
     {
+        if (!grabbable.IsGrabbed) return;
+
+        CalculatePlungerLocation();
+        MovePlunger();
+    }
+
+    private void MovePlunger()
+    {
+        plungerTransform.localPosition = localPosition;
+    }
+
+    private void CalculatePlungerLocation()
+    {
         if (triggerInputValue == 0) return;
+
+        Vector3 targetPosition = Vector3.zero;
 
         // Draw Syringe
         if (drawPlunger && plungerTransform.localPosition.y > maxPlungerDrawValue)
-        {
-            Vector3 targetPosition = new Vector3(plungerTransform.localPosition.x, maxPlungerDrawValue, plungerTransform.localPosition.z);
-            Vector3 translation = (targetPosition - plungerTransform.localPosition).normalized * drawSpeed * triggerInputValue * Runner.DeltaTime;
-
-            if (Vector3.Distance(plungerTransform.localPosition, targetPosition) > translation.magnitude)
-                plungerTransform.Translate(translation);
-            else
-                plungerTransform.localPosition = targetPosition;
-        }
+            targetPosition = new Vector3(plungerTransform.localPosition.x, maxPlungerDrawValue, plungerTransform.localPosition.z);         
         // Push Syringe
         else if (!drawPlunger && plungerTransform.localPosition.y < 0)
-        {
-            Vector3 targetPosition = new Vector3(plungerTransform.localPosition.x, 0f, plungerTransform.localPosition.z);
-            Vector3 translation = (targetPosition - plungerTransform.localPosition).normalized * drawSpeed * triggerInputValue * Runner.DeltaTime;
+            targetPosition = new Vector3(plungerTransform.localPosition.x, 0f, plungerTransform.localPosition.z);
 
-            if (Vector3.Distance(plungerTransform.localPosition, targetPosition) > translation.magnitude)
-                plungerTransform.Translate(translation);
-            else 
-                plungerTransform.localPosition = targetPosition;
-        }
+        // Move Plunger locally
+        Vector3 translation = (targetPosition - plungerTransform.localPosition).normalized * drawSpeed * triggerInputValue * Runner.DeltaTime;
+        plungerTransform.Translate(translation);
+
+        // Save new position for network replication
+        localPosition = plungerTransform.localPosition;
     }
 }
