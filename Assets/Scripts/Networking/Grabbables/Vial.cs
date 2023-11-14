d0691327dc4519494f10be23641211c90d122673
Tyler Latimer <31456150+trlatimer95@users.noreply.github.com>
2023-11-13 22:53:29 -0800

    Cleaned up prefabs, set Vial to follow syringe instead of needle when attached

--- Assets/Scripts/Networking/Grabbables/Vial.cs
+++ Assets/Scripts/Networking/Grabbables/Vial.cs
@@ -1,8 +1,33 @@
+using Fusion;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Vial : MonoBehaviour
+public class Vial : NetworkBehaviour
 {
     [SerializeField] public Material fluidMaterial;
+
+    private NetworkedSocketInteractable socket;
+    private Transform currentOverrideTransform;
+
+    public override void Spawned()
+    {
+        socket = GetComponent<NetworkedSocketInteractable>();
+        if (socket == null)
+            Debug.Log("Unable to find socket interactable on Vial");
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (socket == null || socket.followTransform == null) return;
+
+        if (currentOverrideTransform != null && socket.followTransform == null)
+            currentOverrideTransform = null;
+
+        if (currentOverrideTransform == null || (currentOverrideTransform != null && currentOverrideTransform != socket.followTransform))
+        {
+            currentOverrideTransform = socket.followTransform.GetComponent<NetworkedSocketInteractable>()?.followTransform;
+            socket.followTransform = currentOverrideTransform;
+        }           
+    }
 }
